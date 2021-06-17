a980019f2b576dbdf4c0cc1515dd8f352c48f526
nath248 <nmherrera20@outlook.com>
2021-06-17 13:13:37 -0400

    Syntax edit

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getImage = async function () {
-  const res = await fetch("https://picsum.photos/v2/list?limit=100");
-  const images = await res.json();
-  console.log(images);
+const getImage = async function() {
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    const images = await res.json();
+    console.log(images);
 };
 getImage();
